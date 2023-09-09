2186cf944d06f3c5dd65acc852ebc0dca42e02d0
ZoeSamm <zoemultiply@gmail.com>
2023-09-08 21:00:41 -0700

    api

--- hng_api.py
+++ hng_api.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, request, jsonify
 import datetime
 
@@ -6,35 +7,36 @@ app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
 def get_info():
-    # Get query parameters
-    slack_name = request.args.get('slack_name')
-    track = request.args.get('track')
+    try:
+        slack_name = request.args.get('slack_name')
+        track = request.args.get('track')
 
-    # Get current day of the week (full name)
-    current_day = datetime.datetime.utcnow().strftime('%A')
+        if not slack_name:
+            raise ValueError("Slack name is required.")
+        if not track:
+            raise ValueError("Track is required.")
 
-    # Get current UTC time with +/-2 minute validation
-    current_time = datetime.datetime.utcnow()  # Current UTC time
-    current_time -= datetime.timedelta(minutes=2)  # Subtract 2 minutes
-    current_time_str = current_time.strftime('%Y-%m-%dT%H:%M:%SZ')  # Format as ISO 8601
+        current_day = datetime.datetime.now().strftime('%A')
+        utc_time = datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+        github_file_url = request.url
+        github_repo_url = "https://github.com/ZoeSamm/hng_Api"
 
-    # GitHub URLs
-    github_file_url = 'https://github.com/ZoeSamm/hng_Api/blob/main/file_name.ext'
-    github_repo_url = 'https://github.com/ZoeSamm/hng_Api'
+        response = {
+            "slack_name": slack_name,
+            "current_day": current_day,
+            "utc_time": utc_time,
+            "track": track,
+            "github_file_url": github_file_url,
+            "github_repo_url": github_repo_url,
+            "status_code": 200
+        }
 
-    # JSON response
-    response = {
-        "slack_name": slack_name,
-        "current_day": current_day,
-        "utc_time": current_time_str,
-        "track": track,
-        "github_file_url": github_file_url,
-        "github_repo_url": github_repo_url,
-        "status_code": 200
-    }
+        return jsonify(response)
 
-    return jsonify(response)
+    except Exception as e:
+        error_message = {"error": str(e)}
+        return jsonify(error_message), 400
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
